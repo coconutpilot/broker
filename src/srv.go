0315b759f9da97ed6aaefe5c1f1ca01976ea45ad
David Sparks <david.sparks@gmail.com>
2014-12-29 10:58:07 -0800

    Ran `go fmt`

--- src/srv.go
+++ src/srv.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-    "fmt"
-    "html"
-    "net/http"
+	"fmt"
+	"html"
+	"net/http"
 )
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, html.EscapeString(r.URL.Path))
+	fmt.Fprintf(w, html.EscapeString(r.URL.Path))
 }
 
 func pingHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "pong")
+	fmt.Fprintf(w, "pong")
 }
 
 func main() {
-    http.HandleFunc("/", viewHandler)
-    http.HandleFunc("/ping", pingHandler)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", viewHandler)
+	http.HandleFunc("/ping", pingHandler)
+	http.ListenAndServe(":8080", nil)
 }
