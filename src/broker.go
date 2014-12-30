9145bfe858412d52d6d4896c19209708e29d2559
David Sparks <david.sparks@gmail.com>
2014-12-29 16:02:52 -0800

    Start moving tunables to a config file

--- src/broker.go
+++ src/broker.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"code.google.com/p/gcfg"
 	"fmt"
 	"html"
+	"log"
 	"net/http"
 )
+
+type Config struct {
+	Daemon struct {
+		Port int
+	}
+}
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, html.EscapeString(r.URL.Path))
@@ -15,7 +23,15 @@ func pingHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	var cfg Config
+	err := gcfg.ReadFileInto(&cfg, "broker.cfg")
+	if err != nil {
+		log.Fatalf("Failed to parse gcfg data: %s", err)
+	}
+
+	srv_addr := fmt.Sprintf(":%d", cfg.Daemon.Port)
+
 	http.HandleFunc("/", viewHandler)
 	http.HandleFunc("/ping", pingHandler)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(srv_addr, nil)
 }
