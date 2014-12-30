78ea140402effadeea447c84b3d3d53397e86c9a
David Sparks <david.sparks@gmail.com>
2014-12-29 23:36:24 -0800

    Add functionality to cleanly shutdown.

--- src/broker/broker.go
+++ src/broker/broker.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"code.google.com/p/gcfg"
+	"daemon"
 	"fmt"
 	"html"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 )
 
 type Config struct {
@@ -25,6 +30,14 @@ func pingHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Lshortfile)
+	log.Println("main()")
+	// Start signal handling early (avoid case when signals are delivered before handler installed)
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT)
+	signal.Notify(stop, syscall.SIGHUP)
+	signal.Notify(stop, syscall.SIGTERM)
+
 	var cfg Config
 	err := gcfg.ReadFileInto(&cfg, "broker.cfg")
 	if err != nil {
@@ -33,7 +46,31 @@ func main() {
 
 	srv_addr := fmt.Sprintf(":%d", cfg.Daemon.Port)
 
+	l, err := daemon.New(srv_addr)
+	if err != nil {
+		log.Fatalf("Failed to create listener: %s", err)
+	}
+
 	http.HandleFunc("/", viewHandler)
 	http.HandleFunc("/ping", pingHandler)
-	http.ListenAndServe(srv_addr, nil)
+
+	server := http.Server{}
+
+	var wg sync.WaitGroup
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
+		server.Serve(l)
+	}()
+
+	select {
+	case signal := <-stop:
+		fmt.Printf("Got signal:%v\n", signal)
+	}
+	log.Println("Stopping listener")
+	l.Stop()
+	log.Println("Waiting on server")
+	wg.Wait()
+	log.Println("Exiting")
+
 }
