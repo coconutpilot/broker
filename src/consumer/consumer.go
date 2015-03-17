a4b3aa8515f595909cf1b3adf5275954951f773c
David Sparks <david.sparks@gmail.com>
2015-03-17 01:04:07 -0700

    Updated method to POST.

--- src/consumer/consumer.go
+++ src/consumer/consumer.go
@@ -5,13 +5,21 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
+	"time"
 )
 
 func main() {
-	resp, err := http.Get("http://localhost:8080/ping")
+	client := &http.Client{}
+	time := time.Now()
+
+	req, err := http.NewRequest("POST", "http://localhost:8080/ping", strings.NewReader(time.String()))
+	req.Header.Add("User-Agent", "ElTaco")
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
 
 	if err != nil {
-		log.Fatalf("http.Get => %v", err.Error())
+		log.Fatalf("http.Post => %v", err.Error())
 	}
 	body, _ := ioutil.ReadAll(resp.Body)
 	fmt.Printf("\n%v\n\n", string(body))
